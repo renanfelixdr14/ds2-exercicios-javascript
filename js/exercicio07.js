6e956a0801f1513304fbf5e386b83e68a4bde7ff
renan_felix <renanfe1015@gmail.com>
2026-08-23 03:06:20 -0300

    feat: Piorando o código por causa da atividade 10.

--- js/exercicio07.js
+++ js/exercicio07.js
@@ -2,27 +2,23 @@ const botao7 = document.getElementById("exercicio07");
 
 botao7.addEventListener("click", () => {
     alert("executando Exercicio 07");
-    
-        numeroTentativas = 0;
-        let acertou = false;
 
-        while (numeroTentativas < 3) {
+        let sucesso = false;
+
+        while (sucesso != true) {
+        
             let usuario = prompt("Digite o nome de usuário:");
             let senha = prompt("Digite a senha:");
 
             if (usuario === "admin" && senha === "1234") {
                 alert("Login bem-sucedido!");
-                acertou = true;
-                break;
+                sucesso = true;
+                break
             } else {
-                numeroTentativas++;
-                alert("Usuário ou senha incorretos. Tentativa " + numeroTentativas + " de 3.");
+                alert("Usuário ou senha incorretos.");
             }
         };
 
-        if (!acertou) {
-            alert("Acesso negado! Você excedeu o limite de 3 tentativas.");
-        }
-        
+        // fazendo Down grade no codigo por causa da atividade 10
         //A diferença fundamental é a inteção: "=" guarda um valor em uma variável, enquanto "===" compara dois valores para saber se são idênticos.
 })

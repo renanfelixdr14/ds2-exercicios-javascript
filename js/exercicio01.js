0ca884b85b1ec62156ab84b5c323a16070ca90fa
renan_felix <renanfe1015@gmail.com>
2026-09-01 14:40:21 -0300

    Tirando os prompt.

--- js/exercicio01.js
+++ js/exercicio01.js
@@ -3,16 +3,9 @@ const botao = document.getElementById("exercicio01");
 botao.addEventListener("click", () => {
     alert("executando Exercicio01");
     
-    const nome = prompt("Nome Completo");
-    const curso = prompt("Curso");
-    const funcionalidade = prompt("O que gostaria de desenvolver com JavaScript?");
-
-if (nome != "" & curso != "" & funcionalidade != "") {
-
     alert("JavaScript carregado com sucesso!");
 
-    console.log(`Nome: ${nome}`);
-    console.log(`Curso: ${curso}`);
-    console.log(`O que gostaria de aprender: ${funcionalidade}`);
-};
+    console.log(`Nome: Renan de Arruda Felix`);
+    console.log(`Curso: Sistemas para internet`);
+    console.log(`O que gostaria de aprender: Criar meu proprio site`);
 })
